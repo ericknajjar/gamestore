0016fb157a0b1cae15cb61a49444389e6da539e8
Erick Najjar <ericknajjar@gmail.com>
2016-05-24 15:21:35 -0300

    inicio da localização

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
    end
 
-  helper_method :current_user, :is_admin?
+  helper_method :current_user, :is_admin?, :cookies
 
   def authorize
     redirect_to '/login' unless current_user
@@ -15,6 +15,39 @@ class ApplicationController < ActionController::Base
 
   def is_admin?
     !current_user.blank?
+  end
+
+  before_action :set_locale
+ 
+
+  def set_locale
+    if language_change_necessary?
+      I18n.locale = if params[:locale] 
+         params[:locale] 
+      else 
+        I18n.default_locale
+      end
+
+      set_locale_cookie(I18n.locale)
+    else
+      use_locale_from_cookie
+    end
+
+    puts 'locale: '+params[:locale].to_s
+  end
+  
+
+  def set_locale_cookie(locale)
+    cookies['locale'] = locale.to_s
+  end
+
+ def use_locale_from_cookie
+    I18n.locale = cookies['locale']
+  end
+
+  # A locale change is necessary if no locale cookie is found, or if the locale param has been specified
+  def language_change_necessary?
+    return cookies['locale'].nil? || params[:locale]
   end
 
 end
